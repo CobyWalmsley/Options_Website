3c59c2e3c8aa9a3f18f5c1e0617c91bfb1c8b933
CobyWalmsley <157742907+CobyWalmsley@users.noreply.github.com>
2025-09-25 15:05:30 -0400

    Update Option_Pricing_Calculation.py

--- Option_Pricing_Calculation.py
+++ Option_Pricing_Calculation.py
@@ -196,7 +196,7 @@ else:
         }}
         .lehigh-sticky-banner {{
             position: fixed;
-            top: 9;
+            top: 4;
             left: 16rem;
             right: 0;
             z-index: 9999;
