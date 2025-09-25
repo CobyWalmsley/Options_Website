25ca510fe32d833a9b3c7a637f90e6777159586e
CobyWalmsley <157742907+CobyWalmsley@users.noreply.github.com>
2025-09-25 15:06:50 -0400

    Update Option_Pricing_Calculation.py

--- Option_Pricing_Calculation.py
+++ Option_Pricing_Calculation.py
@@ -192,7 +192,7 @@ else:
     st.markdown(f"""
         <style>
         .block-container {{
-            padding-top: 10rem !important;
+            padding-top: 8rem !important;
         }}
         .lehigh-sticky-banner {{
             position: fixed;
